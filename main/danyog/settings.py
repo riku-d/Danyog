dda371803cfea8f2b4b78145abe7aa84f93d2b11
riku-d <rohitdutta2103@gmail.com>
2024-10-25 21:06:58 +0530

    added files for hosting

--- main/danyog/settings.py
+++ main/danyog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y26gee4mgw4g#p$s8_klt%8@&l0v*_%cpbcfg5rpe&!3x)!kmt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["danyog.onrender.com"]
 
 
 # Application definition
